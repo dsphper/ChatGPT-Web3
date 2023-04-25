ee530ec5d3a5eb1d9762c41f67915a3670d733ba
yunhe <yunhe@66rpg.com>
2023-04-25 15:10:06 +0800

    打印日志

--- service/src/index.ts
+++ service/src/index.ts
@@ -23,6 +23,7 @@ router.post('/chat-process', [auth, limiter], async (req, res) => {
   res.setHeader('Content-type', 'application/octet-stream')
 
   try {
+		console.log(req.body)
     const { prompt, options = {}, systemMessage, temperature, top_p } = req.body as RequestProps
     let firstChunk = true
     await chatReplyProcess({
