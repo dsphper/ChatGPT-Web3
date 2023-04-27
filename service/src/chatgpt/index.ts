17604f4a3c00dc0cc35e6af74da554f80e73f1b4
yunhe <yunhe@66rpg.com>
2023-04-27 15:13:02 +0800

    opz

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -16,7 +16,7 @@ dotenv.config()
 
 const ErrorCodeMessage: Record<string, string> = {
   401: '[OpenAI] 提供错误的API密钥 | Incorrect API key provided',
-	429: '[OpenAI] 正在回答其他人的提问，请您稍等几秒种 | Fuck.',
+	429: '[OpenAI] 正在回答其他人的提问，请您稍等几秒种.',
   403: '[OpenAI] 服务器拒绝访问，请稍后再试 | Server refused to access, please try again later',
   502: '[OpenAI] 错误的网关 |  Bad Gateway',
   503: '[OpenAI] 服务器繁忙，请稍后再试 | Server is busy, please try again later',
